f99e576f0e1f23d582f95d0938459693702c23e9
Matheus Roberto <matheus.datascience@gmail.com>
2022-03-20 10:58:19 -0300

    Part 2: Better Structure with Flask-restful

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask_restful import Api
 from database.db import initialize_db
-from resources.movie import movies
+from resources.routes import initialize_routes
 
+# Flask Configs
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+api = Api(app)
 
-# MongoDB Config
+# MongoDB Configs
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/movie-bag'
-initialize_db(app)
 
-app.register_blueprint(movies)
+# Initialize App
+initialize_db(app)
+initialize_routes(api)
 
 app.run(debug=True)
--- resources/movie.py
+++ resources/movie.py
@@ -1,40 +1,36 @@
-from flask import Blueprint, Response, request
+from flask import Response, request
 from database.db import mongo
 from flask_pymongo import ObjectId
 from bson.json_util import dumps
-
-movies = Blueprint('movies', __name__)
-
-
-@movies.route('/movies', methods=['GET'])
-def get_movies():
-    movies: list = list(mongo.db.movies.find())
-    if not movies:
-        return {'message': 'No movies found.'}
-    return Response(dumps(movies), mimetype='application/json', status=200)
+from flask_restful import Resource
+from typing import Union
 
 
-@movies.route('/movies/<id>', methods=['GET'])
-def get_movie(id):
-    movie = mongo.db.movies.find_one_or_404({'_id': ObjectId(id)})
-    return Response(dumps(movie), mimetype='application/json', status=200)
+class MoviesApi(Resource):
+
+    def get(self: object) -> Union[dict[str, str], Response]:
+        movies: list = list(mongo.db.movies.find())
+        if not movies:
+            return {'message': 'No movies found.'}
+        return Response(dumps(movies), mimetype='application/json', status=200)
+
+    def post(self: object):
+        new_movie = request.get_json()
+        new_movie_id = mongo.db.movies.insert_one(new_movie)
+        return {'_id': str(new_movie_id.inserted_id)}, 200
 
 
-@movies.route('/movies', methods=['POST'])
-def add_movie():
-    new_movie = request.get_json()
-    mongo.db.movies.insert_one(new_movie)
-    return {'id': str(id)}, 200
+class MovieApi(Resource):
 
+    def put(self: object):
+        edited_movie = request.get_json()
+        mongo.db.movies.update_one({'_id': ObjectId(id)}, {'$set': edited_movie})
+        return {'message': 'Movie has been updated.'}, 200
 
-@movies.route('/movies/<id>', methods=['PUT'])
-def update_movie(id):
-    edited_movie = request.get_json()
-    mongo.db.movies.update_one({'_id': ObjectId(id)}, {'$set': edited_movie})
-    return {'message': 'Movie has been updated.'}, 200
+    def delete(self: object, movie_id):
+        mongo.db.movies.delete_one({"_id": ObjectId(movie_id)})
+        return {'message': 'Movie has been deleted.'}, 200
 
-
-@movies.route('/movies/<id>', methods=['DELETE'])
-def delete_movie(id):
-    mongo.db.movies.delete_one({"_id": ObjectId(id)})
-    return '', 200
+    def get(self: object, movie_id) -> Union[dict[str, str], Response]:
+        movie = mongo.db.movies.find_one_or_404({'_id': ObjectId(movie_id)})
+        return Response(dumps(movie), mimetype='application/json', status=200)
